74055c3728896556e14b0a2476595697f5f7627e
shariarhossaintanvir <bangboombro629@gmail.com>
2025-08-30 01:59:05 +0600

    Add Initial Files

---
+++ list_methods.py
@@ -0,0 +1,20 @@
+friends = ["Shariar" , "Oni" , "Saiful" , "Sadiq" , "Younus"]
+
+friends.append("all are friends") #append means add a value in the last
+
+print(friends)
+
+
+cg =[3.12 , 2.34 ,3.98, 2.18]
+
+cg.sort() #this is for sort a value
+cg.reverse() #this is for reverse the value
+cg.insert(2 , "nc") #this is for insert a value in a list , first is index number and second in value 
+
+gayeb = cg.pop(1)
+
+print(gayeb)
+
+friends.remove("Younus")
+print(friends)
+print(cg)
