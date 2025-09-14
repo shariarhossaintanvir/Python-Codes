7bd4cb99aac66ff5fe662e1c5ff9811e264aa241
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-14 15:09:17 +0600

    Add Initial Files

---
+++ Chapter 5_Dictionary and sets/bangla_dictionary.py
@@ -0,0 +1,16 @@
+words = {
+    "oi kire" : "Hey" ,
+    "modhu" : "Honey" ,
+    "python" : "gui sap" ,
+    "code" : "habijabi" ,
+    "hasina" : "soirachar" ,
+    "ice" : "borof" ,
+    "smoke" : "dhoyaaa" ,
+    "chicken" : "murgi" ,
+    "cow" : "goru" ,
+    "crow" : "kak" ,
+    "flag" : "potaka" ,
+    "hand" : "hatt" ,
+}
+word = input("Enter the word you want to translet : ")
+print(words[word])
