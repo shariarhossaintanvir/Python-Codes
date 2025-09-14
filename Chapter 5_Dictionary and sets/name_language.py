e527accdb081dc635a694b23090158a04b30fe0a
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-14 15:22:19 +0600

    Add Initial Files

---
+++ Chapter 5_Dictionary and sets/name_language.py
@@ -0,0 +1,17 @@
+d ={}
+name = input("Enter Your Friends Name : ")
+language = input("Enter Language Name : ")
+d.update({name:language})
+
+name = input("Enter Your Friends Name : ")
+language = input("Enter Language Name : ")
+d.update({name:language})
+
+name = input("Enter Your Friends Name : ")
+language = input("Enter Language Name : ")
+d.update({name:language})
+
+name = input("Enter Your Friends Name : ")
+language = input("Enter Language Name : ")
+d.update({name:language})
+print(d)
