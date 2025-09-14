14891e4580b6fe8870306605365f92ba7ffca87f
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-14 14:46:55 +0600

    Add Initial Files

--- Chapter 5_Dictionary and sets/sets_method.py
+++ Chapter 5_Dictionary and sets/sets_method.py
@@ -1,2 +1,4 @@
 s = {1,2,55,6,7,8,5,4,56, "Shariar"} #there is no repitation value in sets
-print(s,type(s))
+# print(s,type(s))
+s.remove(55)
+print(s)
---
+++ Chapter 5_Dictionary and sets/sets_union_intersection.py
@@ -0,0 +1,4 @@
+s1 = {1 , 3 , 5, 7 , 7 }
+s2 = {1 ,55 ,66 ,43, 3}
+print(s1.union(s2))
+print(s1.intersection(s2))
