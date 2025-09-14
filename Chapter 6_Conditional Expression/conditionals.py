f7becfaf7b494401cd25d5383936b98e61d30a5d
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-14 15:44:57 +0600

    Add Initial Files

---
+++ Chapter 6_Conditional Expression/conditionals.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your Age : "))
+
+if(a>=18):
+    print("It is hightime for learning python")
+
+elif(a<0):
+    print("Invalid Age")
+
+elif(a==0):
+    print("New born baby , drink milk")    
+
+else:
+    print("you have time , take your time to learn")  
+print("Thanks for testing me")      
