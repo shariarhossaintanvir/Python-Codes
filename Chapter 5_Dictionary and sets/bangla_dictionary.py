6714e51008797f45dbdba96f37993ad872581e23
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-16 18:08:32 +0600

    Add Initial Files

--- Chapter 5_Dictionary and sets/bangla_dictionary.py
+++ Chapter 5_Dictionary and sets/bangla_dictionary.py
@@ -8,9 +8,10 @@ words = {
     "smoke" : "dhoyaaa" ,
     "chicken" : "murgi" ,
     "cow" : "goru" ,
-    "crow" : "kak" ,
+    "crow" : "kader kakku" ,
     "flag" : "potaka" ,
     "hand" : "hatt" ,
+    "light" : "aloo aloo" ,
 }
 word = input("Enter the word you want to translet : ")
 print(words[word])
