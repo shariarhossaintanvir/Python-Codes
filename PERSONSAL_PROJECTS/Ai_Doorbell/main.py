53ff805a949158c113d8c19ec01808a7b74bd609
shariarhossaintanvir <bangboombro629@gmail.com>
2025-10-18 14:09:06 +0600

    Add Initial Files

--- PERSONSAL_PROJECTS/Ai_Doorbell/main.py
+++ PERSONSAL_PROJECTS/Ai_Doorbell/main.py
@@ -3,20 +3,19 @@ import face_recognition
 import playsound
 import threading
 cap = cv2.VideoCapture(0)
-sound_played = False 
+sound_played = False
 
 def ring_bell():
     playsound.playsound('doorbell.wav')
 
 while True:
-    _ , frame =cap.read()
+    _ , frame = cap.read()
     faces = face_recognition.face_locations(frame)
-    print(faces)
-    if faces:
+    if faces and not sound_played:
         threading.Thread(target=ring_bell).start()
         sound_played = True
     elif not faces and sound_played:
-        sound_playe = False
-    cv2.imshow('Smart Doorbell',frame)
+        sound_played = False
+    cv2.imshow('Smart Doorbell', frame)
     if cv2.waitKey(10) == ord('q'):
         break
