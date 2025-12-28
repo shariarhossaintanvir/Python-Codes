6610236438e8f96eac158cddd8e16955f84e5062
shariarhossaintanvir <bangboombro629@gmail.com>
2025-12-28 14:31:41 +0600

    Add Initial Files

---
+++ Chapter 7_Loops/for_iterate.py
@@ -0,0 +1,14 @@
+l = [1 , 4, 5, 6, 44, 65, 67 ,89] #listing
+
+for i in l:
+ print(i)
+
+t = (2, 4, 6, 8, 7) #tuple
+
+for i in t:
+    print(i)
+
+s = "Shariar" #string
+
+for i in s:
+    print(i)
