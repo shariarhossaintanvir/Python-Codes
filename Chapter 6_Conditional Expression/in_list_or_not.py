932a67ea0969d821e54f25b22dcdb1ab80058a0b
shariarhossaintanvir <bangboombro629@gmail.com>
2025-12-28 14:12:34 +0600

    Add Initial Files

---
+++ Chapter 6_Conditional Expression/in_list_or_not.py
@@ -0,0 +1,9 @@
+l = ["Shariar", "Oni", "Saiful" , "Sadiq" , "Nirob" , "Younus" , "Minhaz" , "Anik","Azmain"]
+
+
+name = input("Enter your name :")
+
+if(name in l) :
+  print (name)
+else :
+  print("You are not in the close friends list")
