9e1345c2534b35a0a452092fa5d09ba04edce7db
shariarhossaintanvir <bangboombro629@gmail.com>
2025-12-28 14:43:49 +0600

    Add Initial Files

--- Chapter 7_Loops/break.py
+++ Chapter 7_Loops/break.py
@@ -1,4 +1,4 @@
 for i in range(100):
     if (i==70):
-     break
+     break #come out from the loop
     print(i)
---
+++ Chapter 7_Loops/continue.py
@@ -0,0 +1,4 @@
+for i in range(100):
+    if (i==70):
+     continue #skip the iterare and run the program 
+    print(i)
