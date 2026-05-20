81040aa157899ca4ed0e9c28613acf47e52d70d2
shariarhossaintanvir <bangboombro629@gmail.com>
2026-05-20 15:58:15 +0600

    Add Innitial Files

--- Chapter 1_Modul comment and pip/external module.py
+++ Chapter 1_Modul comment and pip/external module.py
@@ -1,2 +1,2 @@
 import pyttsx3
-pyttsx3.speak("I will speak this text")
+pyttsx3.speak("Tanvir is my boss")
--- Chapter 1_Modul comment and pip/print poem.py
+++ Chapter 1_Modul comment and pip/print poem.py
@@ -1,4 +1,4 @@
-print(""" Twinkle twinkle little star
+print( '''Twinkle twinkle little star
 How I wonder what you are
 Up above the world so high
 Like a diamond in the sky
@@ -9,4 +9,4 @@ Shining brightly and afar
 Twinkle star dust all around
 From the sky right to the ground
 Twinkle twinkle little star
-Shining brightly and afar""")
+Shining brightly and afar''')
