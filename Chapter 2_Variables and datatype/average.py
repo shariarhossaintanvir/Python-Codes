d45150ee418a00e50e2ab77772c39f6270c94ffd
shariarhossaintanvir <bangboombro629@gmail.com>
2026-06-02 12:19:00 +0600

    Add Initial Files

--- Chapter 2_Variables and datatype/average.py
+++ Chapter 2_Variables and datatype/average.py
@@ -1,3 +1,5 @@
-a = int(input("Enter the first number : "))
-b = int(input("Enter the second number : "))
-print("The average of given two numbers is :" , (a+b)/2)
+#own written
+a = int(input("Enter the 1st number :"))
+b = int(input("Enter the 1st number :"))
+
+print("average is",(a+b)/2) #formula : a+b/2
